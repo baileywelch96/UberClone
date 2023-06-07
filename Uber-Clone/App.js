75817bc3ab1a4ef74b269617b297ef9ffb69dda7
Bailey <baileywelch1996@hotmail.com>
2023-06-07 22:52:22 +1000

    NavOptions installed

--- Uber-Clone/App.js
+++ Uber-Clone/App.js
@@ -2,13 +2,26 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import { store } from './store';
+import HomeScreen from './screens/HomeScreen';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import MapScreen from './screens/MapScreen';
 
 export default function App() {
+  const Stack = createStackNavigator();
+
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <Stack.Navigator>
+            <Stack.Screen name="HomeScreen" component={HomeScreen} options={{ headerShown: false }} />
+            <Stack.Screen name="MapScreen" component={MapScreen} options={{ headerShown: false }} />
+          </Stack.Navigator>
+        </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
 
   );
