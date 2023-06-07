b0cd0d3ee302eb98fc6642d3a70632874fe7651e
Bailey <baileywelch1996@hotmail.com>
2023-06-07 23:24:51 +1000

    WIP

--- Uber-Clone/screens/HomeScreen.jsx
+++ Uber-Clone/screens/HomeScreen.jsx
@@ -2,10 +2,12 @@ import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native'
 import tw from 'tailwind-react-native-classnames'
 import React from 'react'
 import NavOptions from '../components/NavOptions'
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
+import { GOOGLE_MAPS_API_KEY } from "@env"
 
 const HomeScreen = () => {
   return (
-    <SafeAreaView style={tw`bg-white`}>
+    <SafeAreaView style={tw`bg-white h-full`}>
       <View style={tw`p-5`}>
         <Image 
         style={{
@@ -17,8 +19,13 @@ const HomeScreen = () => {
                 uri: "https://links.papareact.com/gzs"
             }}
         />
+        <GooglePlacesAutocomplete
+            placeholder='Where From?'
+            nearbyPlacesAPI='GooglePlacesSearch'
+            debounce={400}
+        />
+        <NavOptions />
       </View>
-      <NavOptions />
     </SafeAreaView>
   )
 }
